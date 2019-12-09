5c7e187f00d2108505b6c873cb7079a7123f2417
mattlatham-io <matt@mattlatham.io>
2019-12-09 00:50:50 +0000

    Section 3 Complete

--- app/src/app/posts/post-list/post-list.component.ts
+++ app/src/app/posts/post-list/post-list.component.ts
@@ -16,14 +16,15 @@ export class PostListComponent implements OnInit, OnDestroy {
   //   { title: 'Third Post', content: 'This is the third post\'s content' }
   // ]
 
-  storedPosts:Post[] = [];
+  storedPosts: Post[] = [];
   private postsSub: Subscription;
 
   constructor(public postsService:PostsService) { }
 
   ngOnInit() {
 
-    this.storedPosts = this.postsService.getPosts();
+    this.postsService.getPosts();
+
     this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((posts:Post[]) => {
         this.storedPosts = posts;
